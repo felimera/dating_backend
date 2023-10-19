62ef46760f44e65684ebfc8b0d60457286ebc0e8
andres.mera <feli041089@gmail.com>
2023-10-19 14:24:58 -0500

    Agregando la entidades que manejara el reporte.

---
+++ api-dating-appus/src/main/java/com/proyect/apidatingappus/model/entityReport/AssignmentTableReport.java
@@ -0,0 +1,14 @@
+package com.proyect.apidatingappus.model.entityReport;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AssignmentTableReport {
+    private String name;
+    private String description;
+    private String value;
+}
---
+++ api-dating-appus/src/main/java/com/proyect/apidatingappus/model/entityReport/DatingReport.java
@@ -0,0 +1,16 @@
+package com.proyect.apidatingappus.model.entityReport;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DatingReport {
+    private String titleReport;
+    private String customerName;
+    private String customerPhone;
+    private String customerEmail;
+    private String consultationDate;
+    private String consultationTime;
+    private String reportDate;
+}
