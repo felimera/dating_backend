9c4499376decec17e18bead3a399755143696205
andres.mera <feli041089@gmail.com>
2023-11-03 07:05:34 -0500

    AppAssi : Modificacion para el arreglo del CORS en el consumo de endpoint.

--- api-dating-assi/src/main/java/com/proyect/apidatingassi/controller/AssignmentController.java
+++ api-dating-assi/src/main/java/com/proyect/apidatingassi/controller/AssignmentController.java
@@ -7,14 +7,12 @@ import com.proyect.apidatingassi.service.AssignmentService;
 import com.proyect.apidatingassi.util.PreconditionsAssignment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "api/assignment")
 public class AssignmentController {
     @Autowired
