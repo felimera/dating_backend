2375f8540ba0d67b9971bf016112bbb9f3a4a619
andres.mera <feli041089@gmail.com>
2023-11-20 14:42:32 -0500

    AppUs : Creando consulta para la tabla.

--- api-dating-appus/src/main/java/com/proyect/apidatingappus/repository/AppointmentRepository.java
+++ api-dating-appus/src/main/java/com/proyect/apidatingappus/repository/AppointmentRepository.java
@@ -2,8 +2,13 @@ package com.proyect.apidatingappus.repository;
 
 import com.proyect.apidatingappus.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>, AppointmentReportRepository {
+    @Query("select ap from Appointment ap inner join Customer cu on ap.customer.id = cu.id where cu.id = :idCustomer ")
+    List<Appointment> findAllByIdCustomer(Long idCustomer);
 }
