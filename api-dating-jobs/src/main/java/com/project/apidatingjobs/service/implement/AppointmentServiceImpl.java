166877119d3ed04b94278c29ece81b4ef7b62fce
andres.mera <feli041089@gmail.com>
2023-11-25 12:46:05 -0500

    AppJobs : Agregando el servicio de jobs a ejecutar.

--- api-dating-jobs/src/main/java/com/project/apidatingjobs/service/implement/AppointmentServiceImpl.java
+++ api-dating-jobs/src/main/java/com/project/apidatingjobs/service/implement/AppointmentServiceImpl.java
@@ -1,6 +1,27 @@
 package com.project.apidatingjobs.service.implement;
 
+import com.project.apidatingjobs.model.Appointment;
+import com.project.apidatingjobs.repository.AppointmentRepository;
 import com.project.apidatingjobs.service.AppointmentService;
+import com.project.apidatingjobs.util.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class AppointmentServiceImpl implements AppointmentService {
+    private final AppointmentRepository appointmentRepository;
+
+    @Autowired
+    public AppointmentServiceImpl(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    @Override
+    public void statusChangeAppointment() {
+        List<Appointment> appointmentList = appointmentRepository.getOldRecords();
+        appointmentList.forEach(appointment -> appointment.setValid(Constants.F));
+        appointmentRepository.saveAll(appointmentList);
+    }
 }
