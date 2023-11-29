61b713335d09eb8455d0b78e44a6756cea1a32be
andres.mera <feli041089@gmail.com>
2023-11-29 15:06:35 -0500

    AppAssi : Mejoras en el codigo de AssignmentController.java.

--- api-dating-assi/src/main/java/com/proyect/apidatingassi/controller/AssignmentController.java
+++ api-dating-assi/src/main/java/com/proyect/apidatingassi/controller/AssignmentController.java
@@ -3,9 +3,9 @@ package com.proyect.apidatingassi.controller;
 import com.proyect.apidatingassi.controller.dto.AssignmentDto;
 import com.proyect.apidatingassi.controller.dto.AssignmentParameter;
 import com.proyect.apidatingassi.controller.mapper.AssignmentMapper;
+import com.proyect.apidatingassi.exception.precondition.PreconditionsAssignment;
 import com.proyect.apidatingassi.model.Assignment;
 import com.proyect.apidatingassi.service.AssignmentService;
-import com.proyect.apidatingassi.util.PreconditionsAssignment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +18,12 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "api/assignment")
 public class AssignmentController {
+    private AssignmentService assignmentService;
+
     @Autowired
-    AssignmentService assignmentService;
+    public AssignmentController(AssignmentService assignmentService) {
+        this.assignmentService = assignmentService;
+    }
 
     @GetMapping(path = "/all")
     public ResponseEntity<Object> getAll(@RequestParam(defaultValue = "0") String order) {
