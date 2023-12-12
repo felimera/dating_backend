d51394b877e432b87247aefa6321bd2a24c9e611
andres.mera <feli041089@gmail.com>
2023-12-12 11:18:15 -0500

    AppAssi : Creacion de los dto para el accesso a url.

---
+++ api-dating-assi/src/main/java/com/proyect/apidatingassi/controller/dto/RouterLinkDto.java
@@ -0,0 +1,13 @@
+package com.proyect.apidatingassi.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RouterLinkDto {
+    private String label;
+    private String icon;
+    private String url;
+    private String description;
+}
