d4c2057a93df9fb45218c494ae3ffdf7990ef18f
andres.mera <feli041089@gmail.com>
2023-12-12 11:14:33 -0500

    AppUs : Creacion del dto de RouterLink.java

---
+++ api-dating-appus/src/main/java/com/proyect/apidatingappus/controller/dto/RouterLinkDto.java
@@ -0,0 +1,13 @@
+package com.proyect.apidatingappus.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RouterLinkDto {
+    private String label;
+    private String icon;
+    private String url;
+    private String description;
+}
