618e6150af815ff642a21a73e769f1cfed5e9802
andres.mera <feli041089@gmail.com>
2023-12-12 11:16:21 -0500

    AppUs : Validacion de numeros.

--- api-dating-appus/src/main/java/com/proyect/apidatingappus/util/NumberUtils.java
+++ api-dating-appus/src/main/java/com/proyect/apidatingappus/util/NumberUtils.java
@@ -1,10 +1,22 @@
 package com.proyect.apidatingappus.util;
 
+import java.util.regex.Pattern;
+
 public class NumberUtils {
     private NumberUtils() {
-        throw new IllegalStateException("Utility class");
+        throw new IllegalStateException(NumberUtils.class.toString());
     }
+
+    private static Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+
     public static String getFormaterPrice(long price) {
         return "$ " + price + ".00";
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return pattern.matcher(strNum).matches();
     }
 }
