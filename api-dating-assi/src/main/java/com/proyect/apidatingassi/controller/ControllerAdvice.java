ff5b88b024bb4e061684534996ce23eeae33cc9d
andres.mera <feli041089@gmail.com>
2023-12-12 11:20:02 -0500

    AppAssi : Modificacion y creacion del control de exceptiones

--- api-dating-assi/src/main/java/com/proyect/apidatingassi/controller/ControllerAdvice.java
+++ api-dating-assi/src/main/java/com/proyect/apidatingassi/controller/ControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.proyect.apidatingassi.controller;
 
-import com.proyect.apidatingassi.controller.dto.ErrorDto;
+import com.proyect.apidatingassi.controller.dto.MessageDto;
+import com.proyect.apidatingassi.exception.NotFoundException;
 import com.proyect.apidatingassi.exception.RequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,14 +11,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerAdvice {
     @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<ErrorDto> runtimeExceptionHandler(RuntimeException ex) {
-        ErrorDto error = ErrorDto.builder().code("500").message(ex.getMessage()).build();
+    public ResponseEntity<MessageDto> runtimeExceptionHandler(RuntimeException ex) {
+        MessageDto error = MessageDto.builder().code("500").message(ex.getMessage()).build();
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(value = RequestException.class)
-    public ResponseEntity<ErrorDto> requestExceptionHandler(RequestException ex) {
-        ErrorDto error = ErrorDto.builder().code(ex.getCode()).message(ex.getMessage()).build();
+    public ResponseEntity<MessageDto> requestExceptionHandler(RequestException ex) {
+        MessageDto error = MessageDto.builder().code(ex.getCode()).message(ex.getMessage()).build();
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<MessageDto> notFoundExceptionHandler(NotFoundException ex) {
+        MessageDto error = MessageDto.builder().code(ex.getCode()).message(ex.getMessage()).build();
+        return new ResponseEntity<>(error, ex.getHttpStatus());
     }
 }
