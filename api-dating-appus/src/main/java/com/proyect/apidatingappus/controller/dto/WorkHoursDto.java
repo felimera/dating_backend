2c8c0d7d1aa7587c2abfe52d77bc5986987ed940
andres.mera <feli041089@gmail.com>
2023-12-13 13:57:49 -0500

    AppUs : Nueva entidad y dto para las horas laborales.

---
+++ api-dating-appus/src/main/java/com/proyect/apidatingappus/controller/dto/WorkHoursDto.java
@@ -0,0 +1,12 @@
+package com.proyect.apidatingappus.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WorkHoursDto {
+    private Long id;
+    private String value;
+    private Boolean displased;
+}
