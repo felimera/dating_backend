154aafcabef2821a7c3f67c92890eada02217187
andres.mera <feli041089@gmail.com>
2023-12-13 13:57:17 -0500

    AppUs : Modificacion de la consulta por la nueva entidad de tipoRole.

--- api-dating-appus/src/main/java/com/proyect/apidatingappus/repository/implementation/CustomerCriteriaRepositoryImpl.java
+++ api-dating-appus/src/main/java/com/proyect/apidatingappus/repository/implementation/CustomerCriteriaRepositoryImpl.java
@@ -2,6 +2,9 @@ package com.proyect.apidatingappus.repository.implementation;
 
 import com.proyect.apidatingappus.controller.dto.search.CustomerSearchParameterDto;
 import com.proyect.apidatingappus.model.Customer;
+import com.proyect.apidatingappus.model.Customer_;
+import com.proyect.apidatingappus.model.TipoRole;
+import com.proyect.apidatingappus.model.TipoRole_;
 import com.proyect.apidatingappus.model.complement.Rol;
 import com.proyect.apidatingappus.repository.CustomerCriteriaRepository;
 import com.proyect.apidatingappus.util.Constants;
@@ -30,26 +33,28 @@ public class CustomerCriteriaRepositoryImpl implements CustomerCriteriaRepositor
         Root<Customer> customerRoot = cq.from(Customer.class);
         List<Predicate> predicates = new ArrayList<>();
 
+        Join<TipoRole, Customer> tipoRoleCustomerJoin = customerRoot.join(Customer_.TIPO_ROLE, JoinType.INNER);
+
         if (Objects.nonNull(dto.getId()))
-            predicates.add(cb.equal(customerRoot.get("id"), dto.getId()));
+            predicates.add(cb.equal(customerRoot.get(Customer_.ID), dto.getId()));
 
         if (Objects.nonNull(dto.getFillName())) {
-            Expression<String> nameExpression = cb.upper(cb.concat(cb.concat(customerRoot.get("firtName"), Constants.SPACE), customerRoot.get("lastName")));
+            Expression<String> nameExpression = cb.upper(cb.concat(cb.concat(customerRoot.get(Customer_.FIRT_NAME), Constants.SPACE), customerRoot.get(Customer_.LAST_NAME)));
             String nombreParameter = "%".concat(dto.getFillName().toUpperCase()).concat("%");
             predicates.add(cb.like(nameExpression, nombreParameter));
         }
 
         if (Objects.nonNull(dto.getEmail())) {
-            Expression<String> emailExpression = cb.upper(customerRoot.get("email"));
+            Expression<String> emailExpression = cb.upper(customerRoot.get(Customer_.EMAIL));
             String emailParameter = "%".concat(dto.getEmail().toUpperCase()).concat("%");
             predicates.add(cb.like(emailExpression, emailParameter));
         }
 
         if (!predicates.isEmpty()) {
-            predicates.add(cb.notEqual(customerRoot.get("rol"), Rol.A));
+            predicates.add(cb.notEqual(tipoRoleCustomerJoin.get(TipoRole_.ACRONYM), Rol.A.name()));
 
             cq.where(predicates.toArray(new Predicate[0]));
-            cq.orderBy(cb.asc(customerRoot.get("firtName")));
+            cq.orderBy(cb.asc(customerRoot.get(Customer_.FIRT_NAME)));
             return em.createQuery(cq).getResultList();
         }
         return new ArrayList<>();
