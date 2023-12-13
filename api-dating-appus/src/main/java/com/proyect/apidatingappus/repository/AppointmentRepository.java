00f7a52f0d1176f587c5f32ceebae6a752ce9499
andres.mera <feli041089@gmail.com>
2023-12-13 13:55:31 -0500

    AppUs : Nueva consulta para las fecha registras.

--- api-dating-appus/src/main/java/com/proyect/apidatingappus/repository/AppointmentRepository.java
+++ api-dating-appus/src/main/java/com/proyect/apidatingappus/repository/AppointmentRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>, AppointmentReportRepository {
     @Query("select ap from Appointment ap inner join Customer cu on ap.customer.id = cu.id where cu.id = :idCustomer  and ap.valid = 'A' ")
     List<Appointment> findAllByIdCustomer(Long idCustomer);
+
+    @Query("select ap from Appointment ap where ap.date = :selectedDate")
+    List<Appointment> getAllBySelectedDate(LocalDate selectedDate);
 }
